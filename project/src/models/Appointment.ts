53f502732ccb622a633e27af0514e7442d1b7fbd
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-20 16:30:31 -0300

    Cadastro de Usuarios

---
+++ project/src/models/Appointment.ts
@@ -0,0 +1,29 @@
+
+import { Entity, JoinColumn, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, ManyToOne } from 'typeorm';
+import User from './User'
+@Entity('appointments')
+class Appointment {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+
+  provider_id: string;
+
+  @ManyToOne(()=> User)
+  @JoinColumn({name: 'provider_id'})
+  provider: User;
+
+  @Column('time without time zone')
+  date : Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+}
+
+ export default Appointment;
