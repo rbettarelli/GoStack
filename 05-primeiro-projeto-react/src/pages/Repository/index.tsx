cc627222e4c6e7579f20e2cbc1868a814fc130a0
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-24 12:28:33 -0300

    listando Issues da API

--- 05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { Header, RepositoryInfo, Issues } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { FiChevronsRight, FiChevronsLeft } from 'react-icons/fi';
-
+import api from '../../services/api'
 
 interface RepositoryParams {
     repository: string;
 
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string,
+        avatar_url: string,
+    };
+}
+
+interface Issue {
+    id: number;
+    html_url: string;
+    title: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
     const { params } = useRouteMatch<RepositoryParams>();
+
+
+    useEffect(() => {
+
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        });
+
+
+    }, [params.repository]);
 
 
     return (
@@ -23,43 +59,49 @@ const Repository: React.FC = () => {
                     Voltar
                 </Link>
             </Header>
-            <RepositoryInfo>
 
-                <header>
-                    <img src="https://avatars.githubusercontent.com/u/3624098?v=4" alt="Andrew Clark" />
-                    <div>
-                        <strong>facebook/react</strong>
-                        <p>React core at Facebook. Hi!</p>
-                    </div>
-                </header>
-                <ul>
-                    <li>
-                        <strong>1080</strong>
-                        <span>Stars</span>
-                    </li>
-                    <li>
-                        <strong>4000</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>1500</strong>
-                        <span>Issues Abertas </span>
-                    </li>
+            {repository && (
+                <RepositoryInfo>
 
-                </ul>
+                    <header>
+                        <img src={repository.owner.avatar_url}
+                            alt={repository.owner.login} />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Stars</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues Abertas </span>
+                        </li>
 
-            </RepositoryInfo>
+                    </ul>
+
+                </RepositoryInfo>
+            )}
+
 
             <Issues>
+                {issues.map(issue => (
 
-
-                <Link to="sdafdsaf">
+            <a key={issue.id} href={issue.html_url}>
                     <div>
-                        <strong>asfdasasdfasfd</strong>
-                        <p>sgsfsfdsfs</p>
+                        <strong>{issue.title}</strong>
+                        <p>{issue.user.login}</p>
                     </div>
                     <FiChevronsRight size={20} />
-                </Link>
+                </a>
+                ))}
             </Issues>
         </>
     );
