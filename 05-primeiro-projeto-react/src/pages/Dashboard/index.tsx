e9795517aac2ccd94116f62c97b80b1f1fff9603
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-24 11:01:19 -0300

    Navegando entre rotas

--- 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -2,6 +2,7 @@ import React, { useState,useEffect, FormEvent } from 'react';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories, Error } from './style';
 import { FiChevronRight } from 'react-icons/fi';
+import {Link} from 'react-router-dom';
 import api from '../../services/api';
 
 interface Repository {
@@ -81,8 +82,8 @@ const Dashboard: React.FC = () => {
 
             <Repositories>
                 {repositories.map(repository => (
-                     <a key ={repository.full_name}
-                     href='teste'>
+                     <Link key ={repository.full_name}
+                     to={`/repositories/${repository.full_name}`}>
                      <img
                      src={repository.owner.avatar_url} alt={repository.owner.login}
                      />
@@ -91,7 +92,7 @@ const Dashboard: React.FC = () => {
                      <p>{repository.description}</p>
                      </div>
                      <FiChevronRight size={20} />
-                 </a>
+                 </Link>
 
                 ))}
 
