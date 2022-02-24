191564eddc139ebd3b034c7ad9fe0c3bd2e30f2d
Ricardo Bettarelli <ricardo.bettarelli@gmail.com>
2022-02-24 11:45:20 -0300

    Estilizando Repository

--- 05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ 05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import {useRouteMatch} from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronsRight, FiChevronsLeft } from 'react-icons/fi';
 
 
 interface RepositoryParams {
@@ -8,9 +11,58 @@ interface RepositoryParams {
 }
 
 const Repository: React.FC = () => {
-    const {params} = useRouteMatch<RepositoryParams>();
+    const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository:{params.repository} </h1>;
+
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronsLeft size={16} />
+                    Voltar
+                </Link>
+            </Header>
+            <RepositoryInfo>
+
+                <header>
+                    <img src="https://avatars.githubusercontent.com/u/3624098?v=4" alt="Andrew Clark" />
+                    <div>
+                        <strong>facebook/react</strong>
+                        <p>React core at Facebook. Hi!</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1080</strong>
+                        <span>Stars</span>
+                    </li>
+                    <li>
+                        <strong>4000</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>1500</strong>
+                        <span>Issues Abertas </span>
+                    </li>
+
+                </ul>
+
+            </RepositoryInfo>
+
+            <Issues>
+
+
+                <Link to="sdafdsaf">
+                    <div>
+                        <strong>asfdasasdfasfd</strong>
+                        <p>sgsfsfdsfs</p>
+                    </div>
+                    <FiChevronsRight size={20} />
+                </Link>
+            </Issues>
+        </>
+    );
 };
 
 export default Repository;
